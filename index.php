eaf1aef5f0614a59d6e5aad847bac20e1328f43a
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-09 17:21:15 +0200

    forst obj created and printed in html

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php  
-class Movie{
+class Movie
+{
 public $title;
-public $author;
+public $movie_director;
 public $genre;
+
+function __construct($_title, $_movie_director, $_genre){
+$this->title = $_title;
+$this->movie_director = $_movie_director;
+$this->genre = $_genre;
 }
+
+
+}
+$movie1 = new Movie('Il signore degli anelli','Peter Jackson', 'fantasy' );
 
 
 ?>
@@ -20,6 +30,9 @@ public $genre;
     <title>Document</title>
 </head>
 <body>
+    <h1><?= $movie1->title ?></h1>
+    <h2><?= $movie1->movie_director ?></h2>
+    <p><?= $movie1->genre ?></p>
     
 </body>
 </html>
